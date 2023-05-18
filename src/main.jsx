d87a727124e5b6565687d7295103fc35e1add3b4
MohsenSami <mohsensami88@gmail.com>
2023-05-18 10:22:58 +0330

    initial new appp

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import axios from 'axios';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
