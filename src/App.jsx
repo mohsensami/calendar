1a1fd18a41e73cce794d881b355f5fcdf42a7aa8
mohsensami <mohsensami88@gmail.com>
2023-07-11 14:42:55 +0430

    new mui template for home blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { Header } from './components/Header';
-import Posts from './pages/panel/Posts';
-import SinglePost from './pages/panel/SinglePost';
-import CreatePost from './pages/panel/createPost';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header";
+import PostsPanel from "./pages/panel/Posts";
+import Home from "./pages/front/Home";
+import SinglePost from "./pages/panel/SinglePost";
+import CreatePost from "./pages/panel/createPost";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <BrowserRouter>
-                <div className="App">
-                    <Routes>
-                        <Route path="panel" element={<Posts />} />
-                        <Route path="panel/:id" element={<SinglePost />} />
-                        <Route path="panel/create-post" element={<CreatePost />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      {/* <Header /> */}
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="panel" element={<PostsPanel />} />
+            <Route path="panel/:id" element={<SinglePost />} />
+            <Route path="panel/create-post" element={<CreatePost />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
