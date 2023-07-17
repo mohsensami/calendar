ff7272a6f6e9229dc5ffcc5ba9202b4c9cb24d4e
MohsenSami <mohsensami88@gmail.com>
2023-07-17 22:40:58 +0330

    react-router-dom configuration added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Layout from './components/Layout';
 
 function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        <BrowserRouter>
+            <Layout>
+                <div className="App">
+                    <Routes>
+                        {/* <Route path="*" element={<NotFound />} /> */}
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </div>
+            </Layout>
+        </BrowserRouter>
     );
 }
 
