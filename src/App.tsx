98494cc6446e58da24ff5dc5a8deda065a5fc772
MohsenSami <mohsensami88@gmail.com>
2023-07-30 23:14:59 +0330

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import "./App.css";
-import Blog from "./containers/Blog";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import './App.css';
+import Blog from './containers/Blog';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Blog />;
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Blog />
+        </Provider>
+    );
 }
 
 export default App;
