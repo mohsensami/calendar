c69318b679dad51d456cbc6a6f82066fb560a4fe
mohsensami <mohsensami18727@gmail.com>
2024-09-01 23:09:20 +0330

    initial new app => calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoutes from "./utils/PrivateRoutes";
-import Room from "./pages/Room";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import { AuthProvider } from "./utils/AuthContext";
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route element={<PrivateRoutes />}>
-              <Route path="/" element={<Room />} />
-            </Route>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </>
-  );
+
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
 }
 
 export default App;
